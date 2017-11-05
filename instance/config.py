00e01f6c0e2278b694d896b071f442a8496562a7
Shawn Li <shawnli2010@126.com>
2017-11-05 15:39:36 -0800

    finish tutorial part1, finish login,logout, register

--- instance/config.py
+++ instance/config.py
@@ -1,1 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'sqlite:///keniyajie.db'
+SECRET_KEY = 'p9Bv<3Eid9%$i01'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////Users/xueyangli/Desktop/courses/cse210/tritonPetCare/keniyajie.db'
